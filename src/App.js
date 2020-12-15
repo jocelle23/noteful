d4881a37d7aca7d523c46ed2994799fce77f062a
Jocelle Elvambuena <71588038+jocelle23@users.noreply.github.com>
2020-12-14 23:52:34 -0800

    Updating styling and added context

--- src/App.js
+++ src/App.js
@@ -2,96 +2,118 @@ import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
 
 import './App.css';
-import dummyStore from './dummy-store';
 import NavBar from './Components/NavBar';
 import NoteList from './Components/NoteList';
 import NoteView from './Components/NoteView';
-import NoteReturn from './Components/NoteReturn';
+import NoteNav from './Components/NoteNav';
+import APIContext from './APIContext';
 
 export default class App extends Component {
   state = {
-    folders: [],
     notes: [],
+    folders: []
   }
 
   componentDidMount() {
-    this.setState(dummyStore)
+    this.getFolderData();
+    this.getNoteData();
+  }
+
+  getFolderData() {
+    fetch(`http://localhost:9090/folders`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Hey, something went wrong')
+        }
+        return response.json()
+      })
+      .then(data => {
+        this.setState({
+          folders: data
+        })
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  getNoteData() {
+    fetch(`http://localhost:9090/notes`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Hey, something went wrong')
+        }
+        return response.json()
+      })
+      .then(data => {
+        this.setState({
+          notes: data
+        })
+      })
+      .catch(err => {
+        alert(err);
+      })
+  }
+
+  deleteItem = (noteId) => {
+    const filterState = this.state.notes.filter(note => {
+      return note.id === noteId;
+    })
+    this.setState({
+      notes: filterState
+    })
   }
 
   render() {
+    const contextValue = {
+      notes: this.state.notes,
+      folders: this.state.folders,
+      deleteItem: this.deleteItem
+    }
+
     return (
-      <div>
-        <Link to={'/'}>
-          <header>
-            <h1>Noteful</h1>
-          </header>
-        </Link>
+      <div className="App">
+        <APIContext.Provider value={contextValue}>
+          <Link to={'/'}>
+            <header className="App_header">
+              <h1>Noteful</h1>
+            </header>
+          </Link>
 
-        <main>
-          <aside>
-            <Route
-              exact path="/"
-              render={() =>
-                <NavBar
-                  folders={this.state.folders}
-                  notes={this.state.notes}
+          <main>
+            <section className="folderSection">
+              <aside>
+                <Route
+                  exact path="/"
+                  component={NavBar}
                 />
-              }
-            />
-
-            <Route
-            exact path="/folder/:folderId"
-            render={() =>
-              <NavBar
-              folders={this.state.folders}
-              notes={this.state.notes}
-            />
-            }
-            />
-            
-            <Route
-            exact path="/note/:noteId"
-            render={(routerProps) =>
-              <NoteReturn
-              folders = {this.state.folders}
-              notes = {this.state.notes}
-              noteId = {routerProps.match.params.noteId}
-            />
-            }
-            />
-          </aside>
-
-          <section>
-            <Route
-              exact path="/"
-              render={() =>
-                <NoteList
-                  notes={this.state.notes}
+                <Route
+                  exact path="/folder/:folderId"
+                  component={NavBar}
                 />
-              }
-            />
-            <Route
-            exact path="/folder/:folderId"
-            render={(routerProps) =>
-              <NoteList
-              notes={this.state.notes.filter(note =>
-                note.folderId === routerProps.match.params.folderId 
-                )}
-            />
-            }
-            />
-             <Route
-            exact path="/note/:noteId"
-            render={(routerProps) =>
-              <NoteView
-              note={this.state.notes.find(note =>
-                note.id === routerProps.match.params.noteId
-                )}
+                <Route
+                  exact path="/note/:noteId"
+                  component={NoteNav}
+                />
+              </aside>
+            </section>
+
+            <section className="noteSection">
+              <Route
+                exact path="/"
+                component={NoteList}
               />
-            }
-            />
-          </section>
-        </main>
+              <Route
+                exact path="/folder/:folderId"
+                component={NoteList}
+              />
+              <Route
+                exact path="/note/:noteId"
+                component={NoteView}
+              />
+            </section>
+          </main>
+        </APIContext.Provider>
       </div>
     );
   }
